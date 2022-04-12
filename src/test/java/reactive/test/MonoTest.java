33feb7385b5417cdca81c49e4ab110ee6872b9c5
Gustavo Clay <gustavoclay@gmail.com>
2022-04-12 18:38:54 -0300

    setup project

---
+++ src/test/java/reactive/test/MonoTest.java
@@ -0,0 +1,16 @@
+package reactive.test;
+
+import org.junit.jupiter.api.Test;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class MonoTest {
+
+    @Test
+    public void test() {
+        log.info("Everything working as intended");
+
+    }
+
+}
